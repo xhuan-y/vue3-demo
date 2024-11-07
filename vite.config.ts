743e4ae79640d92efe6d4633ce492ca31172c25e
xiaohuan <1305584373@qq.com>
2024-11-07 20:22:59 +0800

    src路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const pathSrc = path.resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 路径别名
+  resolve: {
+    alias: {
+      "@": pathSrc,
+    },
+  },
   plugins: [vue()],
-})
+});
