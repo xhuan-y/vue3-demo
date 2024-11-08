d22092ad93db2187da8f249279401d3afcb3d783
xiaohuan <1305584373@qq.com>
2024-11-08 11:36:09 +0800

    整合pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,30 @@
 <script setup lang="ts">
-import variables from "@/styles/variables.module.scss";
+// import variables from "@/styles/variables.module.scss";
+import { useCounterStore } from "@/store/counter";
+const counterStore = useCounterStore();
 
 defineProps<{ msg: string }>();
 
-const count = ref(0);
-
-console.log(variables.bgColor);
+// console.log(variables.bgColor);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="counterStore.increment">count is {{ counterStore.count }}</button>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   <div>
     <el-button type="success"><i-ep-SuccessFilled />Success</el-button>
     <el-button type="info"><i-ep-InfoFilled />Info</el-button>
     <el-button type="warning"><i-ep-WarningFilled />Warning</el-button>
     <el-button type="danger"><i-ep-WarnTriangleFilled />Danger</el-button>
   </div>
-  <div
+  <!-- <div
     style="width: 100px; height: 100px"
     :style="{ 'background-color': variables.bgColor }"
-  />
+  /> -->
 </template>
 
 <style lang="scss" scoped>
